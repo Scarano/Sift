a5fe3f51ed64cf273c0434b0b0ea4f78401dcffd
Sam Scarano <srs@astoundment.com>
2020-04-30 15:01:43 -0400

    ebay data labeling

---
+++ src/main/scala/Ebay.scala
@@ -0,0 +1,44 @@
+import java.io.File
+
+import scala.io.Source
+
+import Util.printToFile
+
+object Ebay {
+	val patterns = List(
+		"id" -> """(<span class="item__itemid">Item ID: )(\d+)(</span>)""".r,
+		"name" -> """(h3 class="item-title undefined"><a [^>]+><span>)([^<]+)(</span>)""".r,
+		"soldon" -> """(<div class="item__sold-date">Sold on: )(\w\w\w \d+)(</div>)""".r,
+		"itemprice" -> """(<span class="item__price bold">\$)([.\d]+)(</span>)""".r,
+		"totalprice" -> """(<span class="item__order-total bold">\$)([.\d]+)(</span>)""".r,
+	)
+
+	def transform1(line: String): String = {
+		var result = line
+		for ((patternName, pattern) <- patterns) {
+			result = pattern.replaceAllIn(result, "$1❲" + patternName + "❳❮❮$2❯❯$3")
+		}
+		result
+	}
+
+	def transform(line: String): String = {
+		val transformations =
+			for ((patternName, pattern) <- patterns) yield { s: String =>
+				pattern.replaceAllIn(s, "$1❲" + patternName + "❳❮$2❯$3")
+			}
+		transformations.foldLeft(line) { (s, f) => f(s) }
+	}
+
+	def main(args: Array[String]): Unit = {
+		val inputPath = args(0)
+		val outputPath = args(1)
+
+		val source = Source.fromFile(inputPath)
+		try printToFile(new File(outputPath)) { writer =>
+			for (line <- source.getLines) {
+				writer.println(transform(line))
+			}
+		}
+		finally source.close
+	}
+}
---
+++ src/main/scala/EbayTSV.scala
@@ -0,0 +1,93 @@
+import java.io.File
+
+import scala.io.Source
+import Util.printToFile
+
+import scala.annotation.tailrec
+
+object EbayTSV {
+
+	val pattern = """❲(\w+)❳❮([^❯]*)❯""".r
+
+	sealed trait Property
+	case class ID(value: String) extends Property
+	case class Name(value: String) extends Property
+	case class SoldOn(value: String) extends Property
+	case class Price(value: String) extends Property
+	case class TotalPrice(value: String) extends Property
+
+	def findMatches(line: String): List[Property] = pattern.findAllMatchIn(line).map({ m =>
+		if (m.group(1) == "id")
+			ID(m.group(2))
+		else if (m.group(1) == "name")
+			Name(m.group(2))
+		else if (m.group(1) == "soldon")
+			SoldOn(m.group(2))
+		else if (m.group(1) == "itemprice")
+			Price(m.group(2))
+		else if (m.group(1) == "totalprice")
+			TotalPrice(m.group(2))
+		else
+			throw new Exception(s"Unexpected property name '${m.group(1)}''")
+	}).toList
+
+	case class Sale(price: String, date: String, itemIds: List[String], itemNames: List[String])
+
+	@tailrec
+	def process(matches: List[Property],
+	            acc: List[Sale] = List.empty, thisGroup: List[Property] = List.empty)
+	: List[Sale] = {
+
+		def addSale(sales: List[Sale], group: List[Property]): List[Sale] =
+			group.reverse match {
+				case TotalPrice(price) :: SoldOn(date) :: rest =>
+					Sale(price,
+						   date,
+						   rest flatMap { case ID(id) => List(id); case _ => Nil },
+						   rest flatMap { case Name(name) => List(name); case _ => Nil }
+					) :: sales
+				case Nil =>
+					sales
+				case _ =>
+					println(s"Discarding group without price and/or date: $group")
+					sales
+			}
+
+		matches match {
+			case Nil => addSale(acc, thisGroup)
+			case Name(name) :: ID(id) :: Price(price) :: SoldOn(soldOn) :: rest =>
+				process(rest,
+				        Sale(price, soldOn, List(id), List(name)) :: addSale(acc, thisGroup), List.empty)
+			case TotalPrice(price) :: SoldOn(soldOn) :: rest =>
+				process(rest, addSale(acc, thisGroup), List(SoldOn(soldOn), TotalPrice(price)))
+			case Name(name) :: ID(id) :: rest =>
+				process(rest, acc, Name(name) :: ID(id) :: thisGroup)
+			case ID(id) :: rest => // apparently rewgic-sold-2020-04-18-p2.html has a nameless item?
+				println(s"Warning: adding nameless item ID $id")
+				process(rest, acc, Name("???") :: ID(id) :: thisGroup)
+			case _ :: rest =>
+				println("Unable to process: " + matches.take(3))
+				process(rest, addSale(acc, thisGroup), List.empty)
+		}
+	}
+
+	def main(args: Array[String]): Unit = {
+		val inputPath = args(0)
+		val outputPath = args(1)
+
+		val source = Source.fromFile(inputPath)
+		val matches: List[Property] = try source.getLines.flatMap(findMatches).toList
+			finally source.close
+
+		val sales = process(matches).reverse
+
+		printToFile(new File(outputPath)) { writer =>
+			for (sale <- sales) {
+				writer.println(List(sale.price, sale.date,
+				                    sale.itemIds.mkString(";"),
+				                    sale.itemNames.mkString(";")
+				               ).mkString("\t"))
+			}
+		}
+	}
+}
